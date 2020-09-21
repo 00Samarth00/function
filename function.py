136643d9b75d4a25012c9c67e0d262f7410aa79e
00Samarth00 <71335163+00Samarth00@users.noreply.github.com>
2020-09-21 18:52:21 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,28 +1,10 @@
-'''def most_frequent(str1):
-    dict = {}
-    for n in str1:
-        keys = dict.keys()
-        if n in keys:
-            dict[n] += 1
-        else:
-            dict[n] = 1
-    return dict
-
-str1=input("Enter a string:")
-result=most_frequent(str1)
-print(result)'''
-#-------------------------------------------------------------
 
 
 def most_frequent(x):
     alp='abcdefghijklmnopqrstuvwxyz'
-    key=[]
-    val=[]
     result=[]
     for i in range(0,26):
         if(x.count(alp[i])>0):
-            key.append(alp[i])
-            val.append(x.count(alp[i]))
             result.append(str(str(x.count(alp[i]))+'='+alp[i]))
             result.sort(reverse=True)
             
@@ -79,21 +61,7 @@ print(final)
 
 
 
-'''alp='abcdefghijklmnopqrstuvwxyz'
-x={}
-num=[]
-for i in range(0,26):
-    if(in_.count(alp[i])>0):
-       x[alp[i]]=in_.count(alp[i])
-       num.append((in_.count(alp[i]),alp[i]))
-       num.sort(reverse=True)
-       
-       
-    else:
-        None
-print('THE ORDERED PAIR IN FORMAT (NUMBER OF TIMES THAT ALPHABET APPEARS IN THE STRING,ALPHABET):-')
-for r in range(0,len(num)-1):
-    print(num[r])'''
+
     
 
 
